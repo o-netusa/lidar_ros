2c1b49433746ad1c78034500b0460e0749a0dfa7
Qian Xiao <xiaoqian@o-netcom.com>
2023-02-15 06:50:25 +0000

    Update point_types.h

--- src/lidar_ros_driver/point_types.h
+++ src/lidar_ros_driver/point_types.h
@@ -16,9 +16,7 @@ struct PointXYZIRGBT
 {
     PCL_ADD_POINT4D;  // quad-word XYZ
     float intensity;  // point intensity reading
-    float r;          // point r g b
-    float g;
-    float b;
+    uint32_t rgb;     // point r g b
     uint32_t utc_time;                 // point time utc
     uint32_t ms_time;                 // point time ms
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW  // ensure proper alignment
@@ -30,8 +28,6 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(onet::lidar_ros::PointXYZIRGBT,
                                   (float, y, y)
                                   (float, z, z)
                                   (float, intensity, intensity)
-                                  (float, r, r)
-                                  (float, g, g)
-                                  (float, b, b)
+                                  (uint32_t, rgb, rgb)
                                   (uint32_t, utc_time, utc_time)
                                   (uint32_t, ms_time, ms_time))
